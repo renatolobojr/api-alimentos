9fb095c8c86f92c27ef51de00bc5f509d70f9637
renatolobojr <renatolobojr@gmail.com>
2023-02-22 01:02:02 -0300

    pequeno ajuste nas chaves

--- database/seeders/AlimentoSeeder.php
+++ database/seeders/AlimentoSeeder.php
@@ -17,40 +17,41 @@ class AlimentoSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('alimentos')->insert(
-        [   
-            'nome' => 'banana',
-            'tipo'=> 'ouro',
-            'preparo'=> 'cozida',
-            'grupo_id'=> 10,
-            'calorias'=> 78,
-            'proteinas'=> 45,
-            'carboidratos'=> 67,
-            'gorduras'=> 0,
-            'sodio'=> 0.345,
-            'porcao'=> 100,
-            'medida'=> 1,
-            'fator_conversor'=> 1,
-            'fonte_id'=> 2,
-            'created_at'=> now(),
-            'updated_at'=> now()
-        ],[   
-            'nome' => 'maca',
-            'tipo'=> 'gala',
-            'preparo'=> 'crua',
-            'grupo_id'=> 10,
-            'calorias'=> 78,
-            'proteinas'=> 45,
-            'carboidratos'=> 67,
-            'gorduras'=> 0,
-            'sodio'=> 0.345,
-            'porcao'=> 100,
-            'medida'=> 1,
-            'fator_conversor'=> 1,
-            'fonte_id'=> 2,
-            'created_at'=> now(),
-            'updated_at'=> now()
-        ]
-        );
+        DB::table('alimentos')->insert([
+            [  
+                'nome' => 'banana',
+                'tipo'=> 'ouro',
+                'preparo'=> 'cozida',
+                'grupo_id'=> 10,
+                'calorias'=> 78,
+                'proteinas'=> 45,
+                'carboidratos'=> 67,
+                'gorduras'=> 0,
+                'sodio'=> 0.345,
+                'porcao'=> 100,
+                'medida'=> 1,
+                'fator_conversor'=> 1,
+                'fonte_id'=> 2,
+                'created_at'=> now(),
+                'updated_at'=> now()
+            ],
+            [   
+                'nome' => 'maca',
+                'tipo'=> 'gala',
+                'preparo'=> 'crua',
+                'grupo_id'=> 10,
+                'calorias'=> 78,
+                'proteinas'=> 45,
+                'carboidratos'=> 67,
+                'gorduras'=> 0,
+                'sodio'=> 0.345,
+                'porcao'=> 100,
+                'medida'=> 1,
+                'fator_conversor'=> 1,
+                'fonte_id'=> 2,
+                'created_at'=> now(),
+                'updated_at'=> now()
+            ]
+        ]);
     }
 }
