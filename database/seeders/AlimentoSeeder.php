b87cb0698212ca48d59fe0ef3a0791674b4062cb
renatolobojr <renatolobojr@gmail.com>
2023-02-22 19:48:08 -0300

    pequeno ajuste

--- database/seeders/AlimentoSeeder.php
+++ database/seeders/AlimentoSeeder.php
@@ -27,9 +27,8 @@ class AlimentoSeeder extends Seeder
             ['nome' => 'maca', 'tipo'=> 'gala', 'preparo'=> 'crua', 'grupo_id'=> 10,
             'calorias'=> 78, 'proteinas'=> 45, 'carboidratos'=> 67, 'gorduras'=> 0,
             'sodio'=> 0.345, 'porcao'=> 100, 'medida'=> 1, 'fator_conversor'=> 1,
-            'fonte_id'=> 2, 'created_at'=> now(), 'updated_at'=> now()],
+            'fonte_id'=> 2, 'created_at'=> now(), 'updated_at'=> now()]
 
-            ['mamao', 'papaya', 'com casca', 10, 99, 25, 45, 0, 0.123, 100, 1, 1, 2, now(), now()]
         ]);
     }
 }
